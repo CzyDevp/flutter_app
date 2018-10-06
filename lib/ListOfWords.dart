5adb8a8dc0492608f4a34718e3d517126e60c294
czyDevp <navsingh191@gmail.com>
2018-10-06 15:54:25 -0400

    english words with app bar actions added!!

---
+++ lib/ListOfWords.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+class ListOfWords extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Random Words'),
+        ),
+        body: new Center(
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; //save favourite words from list
+  final _bigFont = const TextStyle(fontSize: 18.0); //font size
+  final _saved = new Set<WordPair>(); //storing favourite words here
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Random Word Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list),
+              onPressed: _pushSaved)
+        ], //list icon
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map(
+            (pair) {
+          return new ListTile(
+            title: new Text(
+              pair.asPascalCase,
+              style: _bigFont,
+            ),
+          );
+        },
+      );
+      final divided = ListTile
+          .divideTiles(
+        context: context,
+        tiles: tiles,
+      )
+          .toList();
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Saved Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return new Divider();
+          }
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(
+                generateWordPairs().take(10)); //add 10 new pairs to suggestions
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _bigFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : Colors.green,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/SecondActvity.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_app/ImageView.dart';
+import 'package:flutter_app/ListOfWords.dart';
 void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,14 +30,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   final String title;
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -44,17 +37,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List data;
   String apiUrl = 'https://randomuser.me/api/?results=15';
-  //int _counter = 0;
-  /*void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }*/
   //to make http request and get result as Json
   Future<void>makeRequest() async{
     var response = await http.get(
@@ -80,10 +62,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        //title: new Text(widget.title),
         title: new Text('Contact List'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {
+             // _select(choices[0]);
+              Navigator.push(context,
+                new MaterialPageRoute(
+                    builder: (BuildContext context) => new ListOfWords()),
+              );
+            },
+          ),
+        ],
       ),
       body: new ListView.builder(
         //padding: const EdgeInsets.all(16.0),
@@ -92,9 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
           return new Container(
             decoration: const BoxDecoration(
               border: const Border(
-                //top: const BorderSide(width: 1.0, color: const Color(0xFFFF000000)),
-                //left: const BorderSide(width: 1.0, color: const Color(0xFFFF000000)),
-               // right: const BorderSide(width: 1.0, color: const Color(0xFFFF000000)),
                   bottom: const BorderSide(width: 4.0, color: Colors.deepPurple),
               ),
               ),
@@ -122,40 +110,6 @@ class _MyHomePageState extends State<MyHomePage> {
        );
        },
        child: new Icon(Icons.camera_alt),
-       /*body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),*/ // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
